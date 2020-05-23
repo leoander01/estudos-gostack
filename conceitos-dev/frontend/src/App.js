1a61d92b7f23e801b20c3f10194531c1f972b4e5
Leoander Neves <leoanderneves8@outlook.com>
2020-05-23 20:47:16 -0300

    Criação do ReactJS

--- conceitos-dev/frontend/src/App.js
+++ conceitos-dev/frontend/src/App.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// useState = utilizar o estado atual
+// useEffect = disparar funções
+import api from './services/api';
 
+import './App.css';
+import background from './assets/background.jpg';
 import Header from './components/Header';
 
 /*
 Componente
 Propriedade
-Estado
+Estado & Imutabilidade
 */
 
 function App() {
+    const [projects, setProjects] = useState([]);
+    // useState retorna um array com 2 posições
+    // 1. Variável com o seu valor inicial
+    // 2. Função para atualizarmos esse valor
+
+    useEffect(() => {
+        api.get('/projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject() {
+        // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Leo Neves"
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
+    }
+
     return (
         <>
-            <Header title="Homepage">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                </ul>
-            </Header>
-            <Header title="Projects">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                    <li>Login</li>
-                </ul>
-            </Header>
+            <Header title="Projects" />
+
+            <img width={500} src={background} alt=""/>
+
+            <ul>
+                { projects.map(project => <li key={project.id}>{project.title}</li>) }
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
         </>
     );
 }
 
 export default App;
+
