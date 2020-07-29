7ad019c9d01aae97da86250ae86e0bc263c8ddd0
Leoander Neves <leoanderneves8@outlook.com>
2020-07-29 17:43:32 -0300

    projeto-react

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,10 +1,64 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImage from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 // Tipo da variável (componente), sempre será React.FC
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>
+    return (
+        <>
+            <img src={logoImage} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form action="">
+                <input type="text" placeholder="Digite o nome do repositório..." />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="test">
+                    <img 
+                        src="https://avatars2.githubusercontent.com/u/44913085?s=460&u=eb4989f22dfbedabc90d47da4ac166eca4df09d7&v=4"
+                        alt="Foto de Perfil"
+                    />
+                    <div>
+                        <strong>test/test</strong>
+                        <p>Descrição do repositório</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="test">
+                    <img 
+                        src="https://avatars2.githubusercontent.com/u/44913085?s=460&u=eb4989f22dfbedabc90d47da4ac166eca4df09d7&v=4"
+                        alt="Foto de Perfil"
+                    />
+                    <div>
+                        <strong>test/test</strong>
+                        <p>Descrição do repositório</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="test">
+                    <img 
+                        src="https://avatars2.githubusercontent.com/u/44913085?s=460&u=eb4989f22dfbedabc90d47da4ac166eca4df09d7&v=4"
+                        alt="Foto de Perfil"
+                    />
+                    <div>
+                        <strong>test/test</strong>
+                        <p>Descrição do repositório</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
--- primeiro-projeto-react/src/styles/global.ts
+++ primeiro-projeto-react/src/styles/global.ts
@@ -23,7 +23,7 @@ export default createGlobalStyle`
     #root {
         max-width: 960px;
         margin: 0 auto;
-        padding: 40px 20ox;
+        padding: 40px 20px;
     }
 
     button {
